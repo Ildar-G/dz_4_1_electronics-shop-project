eeac2045d45c17b1bfb747c7011eaf2d39d6a1f3
ildar-G <igazimov83@gmail.com>
2023-09-02 09:29:33 +0300

    Добавить def __repr__() в item.py

--- src/item.py
+++ src/item.py
@@ -82,4 +82,5 @@ class Item:
         """
         self.price *= self.pay_rate
 
-
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
