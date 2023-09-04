f456dba4d2f915309cf7bcdbc4e3cf1a216e5afa
ildar-G <igazimov83@gmail.com>
2023-09-04 21:14:57 +0300

    Добавить test_phone.py тесты: def test_phone_and_phone_add() def test_phone_str_method() def test_add_method_with_invalid_type()

--- src/phone.py
+++ src/phone.py
@@ -16,3 +16,24 @@ class Phone(Item):
 
         return self.quantity + other.quantity
 
+    @property
+    def number_of_sim(self) -> int:
+        """
+                Получение количества поддерживаемых сим-карт.
+        """
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value: int) -> None:
+        """
+        Установка количества поддерживаемых сим-карт.
+        """
+        if value < 1:
+            raise ValueError("Количество SIM-карт должно быть целым неотрицательным числом.")
+        self._number_of_sim = value
+
+    def __repr__(self):
+        """
+        Метод repr для представления объекта в виде строки.
+        """
+        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
