80ab09f2637d22a6de799cdda1c3cdd5cb6d1d6d
ildar-G <igazimov83@gmail.com>
2023-09-12 06:33:11 +0300

    Добавить тесты

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -6,3 +6,12 @@ def test_keyboard_creation():
     assert str(kb) == "Dark Project KD87A"
     assert str(kb.language) == "EN"
 
+
+def test_change_language():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+
+    kb.change_lang()
+    assert str(kb.language) == "RU"
+
+    kb.change_lang().change_lang()
+    assert str(kb.language) == "RU"
