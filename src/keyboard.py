f94d81612fc7cf323b28c12c179fd6de1d8ac2ba
ildar-G <igazimov83@gmail.com>
2023-09-12 05:48:47 +0300

    Создать класс LanguageMixin

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,6 +1,12 @@
 from src.item import Item
 
 
-class Keyboard(Item):
+class LanguageMixin:
+    def __init__(self, language="EN"):
+        self.__language = language
+
+
+class Keyboard(Item, LanguageMixin):
     def __init__(self, name, price, quantity, language="EN"):
         super().__init__(name, price, quantity)
+        LanguageMixin.__init__(self, language)
