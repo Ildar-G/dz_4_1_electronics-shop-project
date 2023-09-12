e58351db3bbfb46c4cb24ebb77d94ca7f1e33baa
ildar-G <igazimov83@gmail.com>
2023-09-12 05:51:20 +0300

    Метод change_lang(self)

--- src/keyboard.py
+++ src/keyboard.py
@@ -5,6 +5,17 @@ class LanguageMixin:
     def __init__(self, language="EN"):
         self.__language = language
 
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == "EN":
+            self.__language = "RU"
+        else:
+            self.__language = "EN"
+        return self
+
 
 class Keyboard(Item, LanguageMixin):
     def __init__(self, name, price, quantity, language="EN"):
